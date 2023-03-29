af96478f9ea4959e622b3cf3c4a0c2cf9415245d
lvneto <lauro.neto.1995@gmail.com>
2023-03-29 15:03:13 -0300

    add type and name at exercises payload

--- src/pages/api/providers/exercises.provider.ts
+++ src/pages/api/providers/exercises.provider.ts
@@ -1,3 +1,5 @@
+import { IExerciseTypesInterface } from './exercises-types.provider'
+import { IExercisesNames } from '@/pages/api/providers/exercises-names.provider'
 import { api } from '../apis/api'
 
 export interface IExerciseInterface {
@@ -9,6 +11,8 @@ export interface IExerciseInterface {
   workoutId?: string
   exerciseNameId?: string
   exerciseTypeId?: string
+  exerciseName?: IExercisesNames
+  exerciseType?: IExerciseTypesInterface
 }
 
 export async function deleteExercise(token: string, id: string): Promise<void> {
