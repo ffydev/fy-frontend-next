5e92a70cc063013cd432e751d7630c64820bae02
lvneto <lauro.neto.1995@gmail.com>
2023-03-30 17:09:12 -0300

    refactoring workouts

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,5 +1,3 @@
-import WorkoutsHeader from '@/components/Workouts/WorkoutsHeader'
-import { WorkoutsLists } from '@/components/Workouts/WorkoutsList'
 import { getUserToken } from '@/pages/api/providers/auth.provider'
 import {
   findPlanTypes,
@@ -10,13 +8,10 @@ import {
   IUserTypeInterface,
 } from '@/pages/api/providers/users-types.provider'
 import { findUsers, IUserInterface } from '@/pages/api/providers/users.provider'
-import {
-  findWorkoutsByUserId,
-  IWorkoutInterface,
-} from '@/pages/api/providers/workouts.provider'
-import { Box, Container, Stack, Tab, TabList, Tabs } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect, useState } from 'react'
+import { Workouts } from '../Workouts'
 import UsersHeader from './UsersHeader'
 import { UsersList } from './UsersList'
 
@@ -29,7 +24,6 @@ export default function Users() {
   const [planTypes, setPlanTypes] = useState<IPlanTypeInterface[]>([])
   const [workoutsComponents, setWorkoutsComponents] = useState<boolean>(false)
   const [userId, setUserId] = useState<string>('')
-  const [userWorkouts, setUserWorkouts] = useState<IWorkoutInterface[]>([])
 
   const fetchUsersData = useCallback(async () => {
     try {
@@ -90,32 +84,6 @@ export default function Users() {
     }
   }, [router, setUserType])
 
-  const fetchUserWorkouts = useCallback(async () => {
-    try {
-      const token = getUserToken()
-
-      if (!token) {
-        // Implementar mensagem personalizada
-        router.push('/login')
-        return
-      }
-
-      const workoutsByUser = await findWorkoutsByUserId(token, userId as string)
-
-      setUserWorkouts(workoutsByUser)
-    } catch (error) {
-      console.error(error)
-      // Implementar mensagem personalizada
-      router.push('/login')
-    }
-  }, [router, userId])
-
-  useEffect(() => {
-    if (userId) {
-      fetchUserWorkouts()
-    }
-  }, [fetchUserWorkouts])
-
   useEffect(() => {
     fetchPlanTypeData()
     fetchUserTypeData()
@@ -138,30 +106,10 @@ export default function Users() {
     <>
       {workoutsComponents ? (
         <>
-          <Box ml={{ base: 0, md: 60 }} minH={'100vh'}>
-            <Container maxW="7xl" p={{ base: 3, md: 10 }}>
-              <WorkoutsHeader
-                fetchUserWorkouts={fetchUserWorkouts}
-                userId={userId}
-                handleWithHideWorkouts={handleWithHideWorkouts}
-              />
-              <Stack maxW={'auto'}>
-                <Tabs>
-                  <TabList>
-                    {userWorkouts?.map((workout: IWorkoutInterface) => (
-                      <Tab key={workout.id}>
-                        Tipo de treino: {workout.workoutType}
-                      </Tab>
-                    ))}
-                  </TabList>
-                  <WorkoutsLists
-                    fetchUserWorkouts={fetchUserWorkouts}
-                    workouts={userWorkouts}
-                  />
-                </Tabs>
-              </Stack>
-            </Container>
-          </Box>
+          <Workouts
+            userId={userId}
+            handleWithHideWorkouts={handleWithHideWorkouts}
+          />
         </>
       ) : (
         <Box
