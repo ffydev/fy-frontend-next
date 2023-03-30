6da4ea4779f8fd44cbc2aabf1109d6bd7d024392
lvneto <lauro.neto.1995@gmail.com>
2023-03-30 14:37:13 -0300

    separeted workouts in tabs

--- src/components/Users/Users/index.tsx
+++ src/components/Users/Users/index.tsx
@@ -1,5 +1,4 @@
 import ExercisesList from '@/components/Exercises/ExercisesList'
-import Feedbacks from '@/components/Feedbacks'
 import PlanList from '@/components/PlanList'
 import { getUserToken } from '@/pages/api/providers/auth.provider'
 import {
@@ -48,6 +47,11 @@ import {
   SimpleGrid,
   Spacer,
   Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
@@ -75,7 +79,6 @@ export default function Users() {
     [],
   )
   const [exerciseNames, setExerciseNames] = useState<IExercisesNames[]>([])
-  const [showFeedback, setShowFeedback] = useState<boolean>(false)
 
   const fetchUsersData = useCallback(async () => {
     try {
@@ -325,14 +328,6 @@ export default function Users() {
     })
   }
 
-  const handleWithShowFeedback = () => {
-    setShowFeedback(true)
-  }
-
-  const handleWithCloseFeedback = () => {
-    setShowFeedback(false)
-  }
-
   return (
     <>
       {workoutsComponents ? (
@@ -392,149 +387,150 @@ export default function Users() {
                 </FormControl>
               </Flex>
               <Stack maxW={'auto'}>
-                {userWorkouts?.map((workout: IWorkoutInterface) => (
-                  <Box
-                    key={workout.id}
-                    p={3}
-                    m={3}
-                    width='100%'
-                    rounded={'lg'}
-                    border={'1px'}
-                    bgColor={'whiteAlpha.50'}
-                    borderColor={'whiteAlpha.100'}
-                    boxShadow={'lg'}
-                    backdropBlur={'1rem'}
-                    backdropFilter='blur(5px)'
-                    minWidth='250px'
-                  >
-                    <Flex>
-                      <Text fontWeight='bold'>
+                <Tabs>
+                  <TabList>
+                    {userWorkouts?.map((workout: IWorkoutInterface) => (
+                      <Tab key={workout.id}>
                         Tipo de treino: {workout.workoutType}
-                      </Text>
-                      <Spacer />
-                      <CloseButton
-                        onClick={() => handleWithDeleteWorkout(workout.id!)}
-                        size='sm'
-                      />
-                    </Flex>
-                    <Button
-                      size='xs'
-                      bgGradient={[
-                        'linear(to-tr, blue.900 20.17%, purple.900 90.87%)',
-                        'linear(to-br, blue.900 20.17%, purple.900 90.87%)',
-                      ]}
-                      onClick={() => handleWithShowFeedback()}
-                    >
-                      Feedback
-                    </Button>
+                      </Tab>
+                    ))}
+                  </TabList>
+                  <TabPanels>
+                    {userWorkouts?.map((workout: IWorkoutInterface) => (
+                      <TabPanel key={workout.id}>
+                        <Box
+                          p={3}
+                          m={3}
+                          width='100%'
+                          rounded={'lg'}
+                          border={'1px'}
+                          bgColor={'whiteAlpha.50'}
+                          borderColor={'whiteAlpha.100'}
+                          boxShadow={'lg'}
+                          backdropBlur={'1rem'}
+                          backdropFilter='blur(5px)'
+                          minWidth='250px'
+                        >
+                          <Flex>
+                            <Text fontWeight='bold'>
+                              Tipo de treino: {workout.workoutType}
+                            </Text>
+                            <Spacer />
+                            <CloseButton
+                              onClick={() =>
+                                handleWithDeleteWorkout(workout.id!)
+                              }
+                              size='sm'
+                            />
+                          </Flex>
 
-                    <Stack direction={['column', 'row']} spacing={6} w={'full'}>
-                      <SimpleGrid
-                        columns={{ base: 1, sm: 2, md: 3 }}
-                        spacing={5}
-                        mt={12}
-                        mb={4}
-                        w={'full'}
-                      >
-                        <FormControl isRequired>
-                          <Select
-                            rounded={'lg'}
-                            variant={'filled'}
-                            bgColor={'blackAlpha.600'}
-                            _hover={{
-                              bgColor: 'blackAlpha.500',
-                              transform: '0.3s',
-                            }}
-                            size={'md'}
-                            w={'auto'}
-                            value={exerciseTypeId}
-                            onChange={(event) =>
-                              setExerciseTypeId(event.target.value)
-                            }
+                          <Stack
+                            direction={['column', 'row']}
+                            spacing={6}
+                            w={'full'}
                           >
-                            <option>Tipo de exercício</option>
-                            {exerciseTypes.map(
-                              (exerciseType: IExerciseTypesInterface) => (
-                                <option
-                                  key={exerciseType.id}
-                                  value={exerciseType.id}
+                            <SimpleGrid
+                              columns={{ base: 1, sm: 2, md: 3 }}
+                              spacing={5}
+                              mt={12}
+                              mb={4}
+                              w={'full'}
+                            >
+                              <FormControl isRequired>
+                                <Select
+                                  rounded={'lg'}
+                                  variant={'filled'}
+                                  bgColor={'blackAlpha.600'}
+                                  _hover={{
+                                    bgColor: 'blackAlpha.500',
+                                    transform: '0.3s',
+                                  }}
+                                  size={'md'}
+                                  w={'auto'}
+                                  value={exerciseTypeId}
+                                  onChange={(event) =>
+                                    setExerciseTypeId(event.target.value)
+                                  }
                                 >
-                                  {exerciseType.name}
-                                </option>
-                              ),
-                            )}
-                          </Select>
-                        </FormControl>
+                                  <option>Tipo de exercício</option>
+                                  {exerciseTypes.map(
+                                    (exerciseType: IExerciseTypesInterface) => (
+                                      <option
+                                        key={exerciseType.id}
+                                        value={exerciseType.id}
+                                      >
+                                        {exerciseType.name}
+                                      </option>
+                                    ),
+                                  )}
+                                </Select>
+                              </FormControl>
 
-                        <FormControl isRequired>
-                          <Select
-                            rounded={'lg'}
-                            variant={'filled'}
-                            bgColor={'blackAlpha.600'}
-                            _hover={{
-                              bgColor: 'blackAlpha.500',
-                              transform: '0.3s',
-                            }}
-                            size={'md'}
-                            w={'auto'}
-                            value={exerciseNameId}
-                            onChange={(event) =>
-                              setExerciseNameId(event.target.value)
-                            }
-                          >
-                            <option>Nome do Exercício</option>
-                            {exerciseNames.map(
-                              (exerciseName: IExercisesNames) => (
-                                <option
-                                  key={exerciseName.id}
-                                  value={exerciseName.id}
+                              <FormControl isRequired>
+                                <Select
+                                  rounded={'lg'}
+                                  variant={'filled'}
+                                  bgColor={'blackAlpha.600'}
+                                  _hover={{
+                                    bgColor: 'blackAlpha.500',
+                                    transform: '0.3s',
+                                  }}
+                                  size={'md'}
+                                  w={'auto'}
+                                  value={exerciseNameId}
+                                  onChange={(event) =>
+                                    setExerciseNameId(event.target.value)
+                                  }
                                 >
-                                  {exerciseName.name}
-                                </option>
-                              ),
-                            )}
-                          </Select>
-                        </FormControl>
+                                  <option>Nome do Exercício</option>
+                                  {exerciseNames.map(
+                                    (exerciseName: IExercisesNames) => (
+                                      <option
+                                        key={exerciseName.id}
+                                        value={exerciseName.id}
+                                      >
+                                        {exerciseName.name}
+                                      </option>
+                                    ),
+                                  )}
+                                </Select>
+                              </FormControl>
 
-                        <Stack>
-                          <Button
-                            ml={3}
-                            size='md'
-                            bgGradient={[
-                              'linear(to-tr, blue.900 20.17%, purple.900 90.87%)',
-                              'linear(to-br, blue.900 20.17%, purple.900 90.87%)',
-                            ]}
-                            onClick={() =>
-                              handleCreateExercise(
-                                workout.id!,
-                                exerciseNameId,
-                                exerciseTypeId,
-                              )
-                            }
-                          >
-                            Adicionar Exercício
-                          </Button>
-                        </Stack>
-                        {workout.exercises && workout.exercises.length > 0 && (
-                          <ExercisesList
-                            fetchUserWorkouts={fetchUserWorkouts}
-                            exercises={workout.exercises}
-                            exerciseNames={exerciseNames}
-                            exerciseTypes={exerciseTypes}
-                          />
-                        )}
-                      </SimpleGrid>
-                    </Stack>
-
-                    {showFeedback ? (
-                      <Feedbacks
-                        userId={userId}
-                        workoutId={workout.id!}
-                        handleWithCloseFeedback={handleWithCloseFeedback}
-                      />
-                    ) : null}
-                  </Box>
-                ))}
+                              <Stack>
+                                <Button
+                                  ml={3}
+                                  size='md'
+                                  bgGradient={[
+                                    'linear(to-tr, blue.900 20.17%, purple.900 90.87%)',
+                                    'linear(to-br, blue.900 20.17%, purple.900 90.87%)',
+                                  ]}
+                                  onClick={() =>
+                                    handleCreateExercise(
+                                      workout.id!,
+                                      exerciseNameId,
+                                      exerciseTypeId,
+                                    )
+                                  }
+                                >
+                                  Adicionar Exercício
+                                </Button>
+                              </Stack>
+                              {workout.exercises &&
+                                workout.exercises.length > 0 && (
+                                  <ExercisesList
+                                    fetchUserWorkouts={fetchUserWorkouts}
+                                    exercises={workout.exercises}
+                                    exerciseNames={exerciseNames}
+                                    exerciseTypes={exerciseTypes}
+                                  />
+                                )}
+                            </SimpleGrid>
+                          </Stack>
+                        </Box>
+                      </TabPanel>
+                    ))}
+                  </TabPanels>
+                </Tabs>
               </Stack>
             </Container>
           </Box>
