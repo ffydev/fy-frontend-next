618d57d9e1c05eb825fccff57a771dd07c6dd87c
maxinelobato <maxinelobato89@hotmail.com>
2023-04-01 20:30:19 -0300

    changes_landingpage

--- src/components/Home/AboutSpecialist/index.tsx
+++ src/components/Home/AboutSpecialist/index.tsx
@@ -1,12 +1,4 @@
-import {
-  Avatar,
-  Box,
-  Container,
-  Flex,
-  Heading,
-  Stack,
-  Text,
-} from '@chakra-ui/react'
+import { Avatar, Box, Container, Flex, chakra, Stack } from '@chakra-ui/react'
 
 export default function AboutSpecialist() {
   const textHeading = {
@@ -14,18 +6,15 @@ export default function AboutSpecialist() {
     heading2:
       'Se você segue sem uma direção ou sem saber o que analisar para identificar seu progresso, tenha a certeza de que você vai se sentir perdido.',
     heading3:
-      'O esforço inteligente é bem direcionado. Do iniciante ao avançado, um diário de treino é essencial na rotina. Através de uma ferramenta assim você consegue obter dados sobre sua força e seu desempenho nos treinos com um simples registro, que pode ser feito através de seu celular, entre os descansos.',
-    heading4: '“bene curris sed extra viam”',
-    heading5:
       'Largue o treino intuitivo, sem direção, e saia de casa sabendo o que irá fazer, volte para casa sabendo o que foi feito e apure semanalmente se você tem evoluído. Seu esforço só vale a pena se este for aplicado na direção certa.',
-    heading6: 'Bons treinos e conte comigo!',
-    heading7: '@andremsena',
+    heading4: 'Bons treinos e conte comigo!',
+    heading5: '@andremsena',
   }
   return (
-    <Box bgGradient={'linear(to-l, orange.500, blackAlpha.400)'}>
+    <>
       <Container maxW={'7xl'} p={8}>
         <Stack direction={{ base: 'column', md: 'row' }}>
-          <Flex flex={1} justifyContent='center' align='center'>
+          <Flex flex={1} justifyContent="center" align="center">
             <Box p={6}>
               <Stack
                 spacing={2}
@@ -33,62 +22,55 @@ export default function AboutSpecialist() {
                 maxW={'lg'}
                 textAlign={{ base: 'center', md: 'left' }}
               >
-                <Heading
-                  fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
-                  textShadow={'#000 1px 1px'}
+                <chakra.h1
+                  fontSize={['2xl', '4xl']}
+                  lineHeight={1.4}
+                  fontWeight="bold"
+                  color={'whiteAlpha.900'}
                 >
                   {textHeading.heading1}
-                </Heading>
-                <Text
-                  fontSize={{ base: 'sm', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
+                </chakra.h1>
+                <chakra.h2
+                  color="whiteAlpha.800"
+                  fontSize={['lg', 'xl']}
+                  lineHeight={1.4}
                 >
                   {textHeading.heading2}
-                </Text>
-                <Text
-                  fontSize={{ base: 'sm', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
+                </chakra.h2>
+                <chakra.h2
+                  color="whiteAlpha.800"
+                  fontSize={['lg', 'xl']}
+                  lineHeight={1.4}
                 >
                   {textHeading.heading3}
-                </Text>
-                <Text
-                  fontSize={{ base: 'md', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
+                </chakra.h2>
+                <chakra.h2
+                  color="whiteAlpha.800"
+                  fontSize={['lg', 'xl']}
+                  lineHeight={1.4}
                 >
                   {textHeading.heading4}
-                </Text>
-                <Text
-                  fontSize={{ base: 'md', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
+                </chakra.h2>
+                <chakra.h2
+                  color="whiteAlpha.500"
+                  fontSize={['lg', 'xl']}
+                  lineHeight={1.2}
                 >
                   {textHeading.heading5}
-                </Text>
-                <Text
-                  fontSize={{ base: 'md', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
-                >
-                  {textHeading.heading6}
-                </Text>
-                <Text
-                  fontSize={{ base: 'md', lg: 'lg' }}
-                  color={'whiteAlpha.800'}
-                >
-                  {textHeading.heading7}
-                </Text>
+                </chakra.h2>
               </Stack>
             </Box>
           </Flex>
           <Flex flex={1} align={'center'} justify={'center'}>
             <Avatar
               src={'/andresena.webp'}
-              size={'6xl'}
+              size="3xl"
               loading={'lazy'}
-              border={'8px'}
-              borderColor={'whiteAlpha.900'}
+              shadow={'dark-lg'}
             />
           </Flex>
         </Stack>
       </Container>
-    </Box>
+    </>
   )
 }
