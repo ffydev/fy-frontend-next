f7ab303e2129deefaed08b6aa7abc7737bf9b518
lvneto <lauro.neto.1995@gmail.com>
2023-04-02 10:38:26 -0300

    fix position loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,13 +3,15 @@ import { Spinner } from '@chakra-ui/react'
 export default function Loading() {
   return (
     <>
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-      />
+      <div style={{ position: 'fixed', bottom: '10px', right: '10px' }}>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </div>
     </>
   )
 }
