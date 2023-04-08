a29911f6b5500f5480070e7ffa7d5517fe973da1
lvneto <lauro.neto.1995@gmail.com>
2023-04-08 06:54:24 -0300

    creating dashboard to user

--- src/components/NavigationUser/Dashboard/index.tsx
+++ src/components/NavigationUser/Dashboard/index.tsx
@@ -1,8 +1,13 @@
+import { Box, Heading } from '@chakra-ui/react'
+
 export default function Dashboard() {
   return (
     <>
-      {' '}
-      <h1>Componentes do menu de usuário</h1>
+      <Box ml={{ base: 0, md: 60 }} m={4} minH={'100vh'}>
+        <Heading as="h3" size="lg" mb="4" fontWeight="medium" textAlign="left">
+          Dashboard
+        </Heading>
+      </Box>
     </>
   )
 }
