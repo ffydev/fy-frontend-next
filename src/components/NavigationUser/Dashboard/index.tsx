641659c6cf876ce4d9ab9fc3b08a854ed020d7da
lvneto <lauro.neto.1995@gmail.com>
2023-04-11 15:07:52 -0300

    flow anamnesis working

--- src/components/NavigationUser/Dashboard/index.tsx
+++ src/components/NavigationUser/Dashboard/index.tsx
@@ -4,19 +4,30 @@ import { useContext, useState } from 'react'
 import { Workouts } from '../Workouts'
 import HandleButton from '@/components/Buttons/HandleButton'
 import { ArrowArcLeft } from '@phosphor-icons/react'
+import AnamnesisCreate from '../Anamnesis/AnamnesisCreate'
 
 export default function Dashboard() {
-  const { isShowingWorkouts, handleWithShowWorkouts } =
-    useContext(ContextDashboardUser)
+  const {
+    isShowingWorkouts,
+    isShowAnamnesis,
+    handleWithShowWorkouts,
+    handleWithShowAnamnesis,
+  } = useContext(ContextDashboardUser)
   const [isShowingDashboard, setIsShowingDashboard] = useState<boolean>(true)
 
   const handleWithDashboard = () => {
     handleWithShowWorkouts(false)
+    handleWithShowAnamnesis(false)
     setIsShowingDashboard(true)
   }
 
   const handleWithWorkouts = () => {
     handleWithShowWorkouts(true)
+    setIsShowingDashboard(false)
+  }
+
+  const handleWithAnamnesis = () => {
+    handleWithShowAnamnesis(true)
     setIsShowingDashboard(false)
   }
 
@@ -35,6 +46,23 @@ export default function Dashboard() {
                 size={'xl'}
                 src={
                   'https://images.unsplash.com/photo-1576678927484-cc907957088c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+                }
+                css={{
+                  border: '2px solid white',
+                }}
+              />
+            </Button>
+
+            <Button
+              background={'none'}
+              _hover={{}}
+              _active={{}}
+              onClick={() => handleWithAnamnesis()}
+            >
+              <Avatar
+                size={'xl'}
+                src={
+                  'https://plus.unsplash.com/premium_photo-1661483120409-accf4c0ebd8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1954&q=80'
                 }
                 css={{
                   border: '2px solid white',
@@ -64,6 +92,26 @@ export default function Dashboard() {
           <Workouts />
         </>
       )}
+
+      {isShowAnamnesis ? (
+        <>
+          <Stack
+            direction={'column'}
+            align={'start'}
+            alignSelf={'center'}
+            position={'relative'}
+            mt={3}
+            ml={3}
+          >
+            <HandleButton
+              text={'Voltar'}
+              leftIcon={<ArrowArcLeft size={28} weight="bold" />}
+              onClick={() => handleWithDashboard()}
+            />
+          </Stack>
+          <AnamnesisCreate />
+        </>
+      ) : null}
     </>
   )
 }
--- src/hooks/ContextDashboardUser/index.tsx
+++ src/hooks/ContextDashboardUser/index.tsx
@@ -2,12 +2,16 @@ import React, { createContext, useState } from 'react'
 
 export interface ContextData {
   isShowingWorkouts: boolean
+  isShowAnamnesis: boolean
   handleWithShowWorkouts(isShowingWorkouts: boolean): void
+  handleWithShowAnamnesis(isShowAnamnesis: boolean): void
 }
 
 export const ContextDashboardUser = createContext<ContextData>({
   isShowingWorkouts: false,
+  isShowAnamnesis: false,
   handleWithShowWorkouts: () => {},
+  handleWithShowAnamnesis: () => {},
 })
 
 type ContextDashboardUserProviderProps = {
@@ -18,16 +22,23 @@ export default function ContextDashboardUserProvider({
   children,
 }: ContextDashboardUserProviderProps) {
   const [isShowingWorkouts, setIsShowingWorkouts] = useState<boolean>(false)
+  const [isShowAnamnesis, setIsShowAnamnesis] = useState<boolean>(false)
 
   const handleWithShowWorkouts = (isShowingWorkouts: boolean) => {
     setIsShowingWorkouts(isShowingWorkouts)
+  }
+
+  const handleWithShowAnamnesis = (isShowAnamnesis: boolean) => {
+    setIsShowAnamnesis(isShowAnamnesis)
   }
 
   return (
     <ContextDashboardUser.Provider
       value={{
         isShowingWorkouts,
+        isShowAnamnesis,
         handleWithShowWorkouts,
+        handleWithShowAnamnesis,
       }}
     >
       {children}
