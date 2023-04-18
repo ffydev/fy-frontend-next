56b3be5c28ec8f017d55b23ffc2888cf2cf0a561
lvneto <lauro.neto.1995@gmail.com>
2023-04-18 14:05:14 -0300

    creating and update feedbacks ok

--- src/components/NavigationAdmin/Feedbacks/index.tsx
+++ src/components/NavigationAdmin/Feedbacks/index.tsx
@@ -1,8 +1,9 @@
 import HandleButton from '@/components/Buttons/HandleButton'
+import { useAuth } from '@/hooks/ContextAuth'
 import { ContextDashboardAdmin } from '@/hooks/ContextDashboardAdmin'
 import { getUserToken } from '@/pages/api/providers/auth.provider'
-import { answerFeedback } from '@/pages/api/providers/doctor.feedbacks'
 import {
+  answerFeedback,
   findUserFeedbacks,
   IUserFeedback,
 } from '@/pages/api/providers/user-feedbacks.provider'
@@ -13,6 +14,7 @@ import { useCallback, useContext, useEffect, useState } from 'react'
 
 export default function Feedbacks() {
   const router = useRouter()
+  const { user } = useAuth()
   const { userId, setIsShowingFeedbacks, setIsShowingUsers } = useContext(
     ContextDashboardAdmin,
   )
@@ -41,7 +43,7 @@ export default function Feedbacks() {
     fetchFeedbacksData()
   }, [fetchFeedbacksData])
 
-  const handleWithAswerFeedback = async () => {
+  const handleWithAswerFeedback = async (id: string) => {
     try {
       const token = getUserToken()
 
@@ -51,8 +53,9 @@ export default function Feedbacks() {
         return
       }
 
-      await answerFeedback(token, {
-        userId,
+      await answerFeedback(token, id, {
+        doctorId: user?.id,
+        isAnswered: true,
         answer,
       })
     } catch (error) {
@@ -98,19 +101,29 @@ export default function Feedbacks() {
             Outros: {feedback.others}
           </chakra.h1>
 
-          <FormControl mt={4}>
-            <Textarea
-              placeholder="Responda aqui"
-              onChange={(event) => setAnswer(event.target.value)}
-            />
-            <Flex justifyContent={'center'} w={'full'} mt={3}>
-              <HandleButton
-                text={'Responder'}
-                leftIcon={<Plus size={28} weight="bold" />}
-                onClick={handleWithAswerFeedback}
-              />
-            </Flex>
-          </FormControl>
+          {feedback.isAnswered ? (
+            <>
+              <chakra.h1 fontSize="lg" lineHeight={6} mb={3}>
+                Status: Respondido
+              </chakra.h1>
+            </>
+          ) : (
+            <>
+              <FormControl mt={4}>
+                <Textarea
+                  placeholder="Responda aqui"
+                  onChange={(event) => setAnswer(event.target.value)}
+                />
+                <Flex justifyContent={'center'} w={'full'} mt={3}>
+                  <HandleButton
+                    text={'Responder'}
+                    leftIcon={<Plus size={28} weight="bold" />}
+                    onClick={() => handleWithAswerFeedback(feedback.id!)}
+                  />
+                </Flex>
+              </FormControl>
+            </>
+          )}
         </Box>
       ))}
     </>
--- src/pages/api/providers/user-feedbacks.provider.ts
+++ src/pages/api/providers/user-feedbacks.provider.ts
@@ -8,7 +8,10 @@ export interface IUserFeedback {
   fatigue?: string
   others?: string
   videoLinks?: string
+  answer?: string
   userId?: string
+  doctorId?: string
+  isAnswered?: boolean
   createdAt?: string
 }
 
@@ -47,6 +50,23 @@ export async function createUserFeedback(
     return response.data
   } catch (error) {
     console.error('Failed to find users', error)
+    throw error
+  }
+}
+
+export async function answerFeedback(
+  token: string,
+  id: string,
+  feedback: IUserFeedback,
+): Promise<IUserFeedback> {
+  try {
+    const response = await api.patch(`/user-feedbacks/${id}`, feedback, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+
+    return response.data
+  } catch (error) {
+    console.error('Failed to create user', error)
     throw error
   }
 }
