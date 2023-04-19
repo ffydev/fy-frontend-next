a0028cf56b43cf2f715f1cf02c718fd11cbb7a86
lvneto <lauro.neto.1995@gmail.com>
2023-04-19 13:09:50 -0300

    refactoring

--- src/components/NavigationAdmin/NavigationAdmin.tsx
+++ src/components/NavigationAdmin/NavigationAdmin.tsx
@@ -1,30 +1,15 @@
-import { getUserToken } from '@/pages/api/providers/auth.provider'
-import {
-  findPlansTypes,
-  IPlanType,
-} from '@/pages/api/providers/plans-types.provider'
-import { findUsers, IUserInterface } from '@/pages/api/providers/users.provider'
 import { Box, Container, Stack } from '@chakra-ui/react'
 import { ArrowArcLeft } from '@phosphor-icons/react'
-import { useRouter } from 'next/router'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import HandleButton from '../Buttons/HandleButton'
 import { Workouts } from './Workouts'
-import UsersHeader from './UsersHeader'
-import { UsersList } from './UsersList'
 import Feedbacks from './Feedbacks'
 import WorkoutsHeader from './Workouts/WorkoutsHeader'
 import ListAnamnesis from './Anamnesis'
 import { useAdminNavigationStore } from '@/hooks/AdminNavigationStore/admin.navigation.store'
+import Users from './Users'
 
 export default function NavigationAdmin() {
-  const router = useRouter()
-  const [users, setUsers] = useState<IUserInterface[]>([])
-  const [userTypeId, setUserTypeId] = useState<string>('')
-  const [search, setSearch] = useState<string>('')
-  const [isDeleted, setIsDeleted] = useState<string>('')
-  const [planTypes, setPlanTypes] = useState<IPlanType[]>([])
-
   const {
     isShowingUsers,
     isShowingWorkouts,
@@ -37,51 +22,6 @@ export default function NavigationAdmin() {
     setSelectedUserId,
     reset,
   } = useAdminNavigationStore()
-
-  const fetchUsersData = useCallback(async () => {
-    try {
-      const token = getUserToken()
-
-      if (!token) {
-        // Implementar mensagem personalizada
-        return router.push('/login')
-      }
-
-      const response = await findUsers(token, {
-        userTypeId,
-        search,
-        isDeleted,
-      })
-      setUsers(response)
-    } catch (error) {
-      console.error(error)
-      router.push('/login')
-    }
-  }, [router, userTypeId, search, isDeleted])
-
-  const fetchPlanTypeData = useCallback(async () => {
-    try {
-      const token = getUserToken()
-
-      if (!token) {
-        // Implementar mensagem personalizada
-        router.push('/login')
-        return
-      }
-
-      const response = await findPlansTypes(token)
-
-      setPlanTypes(response)
-    } catch (error) {
-      console.error(error)
-      router.push('/login')
-    }
-  }, [router, setPlanTypes])
-
-  useEffect(() => {
-    fetchUsersData()
-    fetchPlanTypeData()
-  }, [fetchUsersData, fetchPlanTypeData])
 
   const handleWithHideWorkouts = () => {
     setIsShowingWorkouts()
@@ -113,20 +53,7 @@ export default function NavigationAdmin() {
         <>
           <Box ml={{ base: 0, md: 60 }} minH={'100vh'}>
             <Container maxW="7xl" p={{ base: 5, md: 10 }}>
-              <UsersHeader
-                fetchUsersData={fetchUsersData}
-                planTypes={planTypes}
-                search={search}
-                setUserTypeId={setUserTypeId}
-                setSearch={setSearch}
-                setIsDeleted={setIsDeleted}
-              />
-
-              <UsersList
-                fetchUsersData={fetchUsersData}
-                users={users}
-                planTypes={planTypes}
-              />
+              <Users />
             </Container>
           </Box>
         </>
---
+++ src/components/NavigationAdmin/Users/index.tsx
@@ -0,0 +1,84 @@
+import UsersHeader from '../UsersHeader'
+import { useCallback, useEffect, useState } from 'react'
+import {
+  IPlanType,
+  findPlansTypes,
+} from '@/pages/api/providers/plans-types.provider'
+import { getUserToken } from '@/pages/api/providers/auth.provider'
+import { IUserInterface, findUsers } from '@/pages/api/providers/users.provider'
+import { useRouter } from 'next/router'
+import { UsersList } from '../UsersList'
+
+export default function Users() {
+  const router = useRouter()
+  const [users, setUsers] = useState<IUserInterface[]>([])
+  const [userTypeId, setUserTypeId] = useState<string>('')
+  const [search, setSearch] = useState<string>('')
+  const [isDeleted, setIsDeleted] = useState<string>('')
+  const [planTypes, setPlanTypes] = useState<IPlanType[]>([])
+
+  const fetchUsersData = useCallback(async () => {
+    try {
+      const token = getUserToken()
+
+      if (!token) {
+        // Implementar mensagem personalizada
+        return router.push('/login')
+      }
+
+      const response = await findUsers(token, {
+        userTypeId,
+        search,
+        isDeleted,
+      })
+
+      setUsers(response)
+    } catch (error) {
+      console.error(error)
+      router.push('/login')
+    }
+  }, [router, userTypeId, search, isDeleted])
+
+  const fetchPlanTypeData = useCallback(async () => {
+    try {
+      const token = getUserToken()
+
+      if (!token) {
+        // Implementar mensagem personalizada
+        router.push('/login')
+        return
+      }
+
+      const response = await findPlansTypes(token)
+
+      setPlanTypes(response)
+    } catch (error) {
+      console.error(error)
+      router.push('/login')
+    }
+  }, [router, setPlanTypes])
+
+  useEffect(() => {
+    fetchUsersData()
+    fetchPlanTypeData()
+  }, [fetchUsersData, fetchPlanTypeData])
+
+  return (
+    <>
+      <UsersHeader
+        fetchUsersData={fetchUsersData}
+        planTypes={planTypes}
+        search={search}
+        setUserTypeId={setUserTypeId}
+        setSearch={setSearch}
+        setIsDeleted={setIsDeleted}
+      />
+
+      <UsersList
+        fetchUsersData={fetchUsersData}
+        users={users}
+        planTypes={planTypes}
+      />
+    </>
+  )
+}
