f5461cfc1ba985c4b6378f2093d42b10ea1c6c23
lvneto <lauro.neto.1995@gmail.com>
2023-04-30 08:36:12 -0300

    changing layout

--- src/components/UserFlow/Workouts/WorkoutsList.tsx
+++ src/components/UserFlow/Workouts/WorkoutsList.tsx
@@ -10,19 +10,7 @@ export function WorkoutsList({ workouts }: WorkoutsProps) {
   return (
     <>
       {workouts?.map((workout: IWorkout) => (
-        <Box
-          key={workout.id}
-          p={4}
-          width="100%"
-          rounded={'lg'}
-          border={'1px'}
-          bgColor={'whiteAlpha.50'}
-          borderColor={'whiteAlpha.100'}
-          boxShadow={'lg'}
-          backdropBlur={'1rem'}
-          backdropFilter="blur(5px)"
-          minWidth="250px"
-        >
+        <Box key={workout.id} p={4} width="100%">
           <Stack direction={['column', 'row']} w={'full'}>
             <SimpleGrid columns={{ base: 1, md: 3 }} spacing={6} w={'full'}>
               {workout.exercises && workout.exercises.length > 0 && (
