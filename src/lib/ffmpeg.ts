70c91514f27e39e48aa46fed18af3e502496afa0
lvneto <lauro.neto.1995@gmail.com>
2023-07-19 07:36:33 -0300

    changign path

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -2,5 +2,5 @@ import { createFFmpeg } from '@ffmpeg/ffmpeg'
 
 export const ffmpeg = createFFmpeg({
   log: false,
-  corePath: 'http://localhost:3000/ffmpeg-dist/ffmpeg-core.js',
+  corePath: '/ffmpeg-dist/ffmpeg-core.js',
 })
