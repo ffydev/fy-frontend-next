ca42b6053b4acda2c0917091cc001584bb40dfce
lvneto <lauro.neto.1995@gmail.com>
2023-08-05 15:20:00 -0300

    creating exercises Menu

--- src/components/Dashboards/DashboardMenuOwner.tsx
+++ src/components/Dashboards/DashboardMenuOwner.tsx
@@ -35,6 +35,7 @@ export default function DashboardMenuOwner() {
 
   const handleWithShowDashboardHome = () => {
     setDashboardHome(true)
+    setDashboardExercises(false)
   }
 
   const handleWithShowDashboardExercises = () => {
