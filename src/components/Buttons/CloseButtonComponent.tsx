119f6df750bb93f2dd46ee7f4967dc5c16b66ead
lvneto <lauro.neto.1995@gmail.com>
2023-08-21 13:39:44 -0300

    confirmation to delete

--- src/components/Buttons/CloseButtonComponent.tsx
+++ src/components/Buttons/CloseButtonComponent.tsx
@@ -1,4 +1,14 @@
-import { CloseButton } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {
+  CloseButton,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
+} from '@chakra-ui/react'
 
 interface CloseButtonProps {
   key?: string
@@ -12,20 +22,58 @@ interface CloseButtonProps {
   mt?: number
   mb?: number
   ml?: number
+  onDelete?: () => void
 }
 
-export function CloseButtonComponent({ onClick, ...rest }: CloseButtonProps) {
+export function CloseButtonComponent({
+  onDelete,
+  onClick,
+  ...rest
+}: CloseButtonProps) {
+  const [isOpen, setIsOpen] = useState(false)
+  const cancelRef = React.useRef()
+
+  const onClose = () => setIsOpen(false)
+  const onDeleteClick = () => {
+    setIsOpen(true)
+  }
+
+  const handleDeleteConfirm = () => {
+    onDelete?.()
+    onClick?.()
+    onClose()
+  }
+
   return (
-    <CloseButton
-      _hover={{
-        bgGradient: 'linear(to-r, red.500, red.600)',
-        transition: '0.8s',
-      }}
-      size="sm"
-      border={'1px'}
-      borderColor={'whiteAlpha.300'}
-      onClick={onClick}
-      {...rest} // Propagação das demais propriedades aqui
-    />
+    <>
+      <CloseButton onClick={onDeleteClick} {...rest} />
+
+      <AlertDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        leastDestructiveRef={cancelRef as any}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Confirmação de exclusão
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Você tem certeza que deseja deletar?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button colorScheme="gray" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={handleDeleteConfirm} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   )
 }
